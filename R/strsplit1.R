f8050c62ec00180e92ced603aaeb337f3d457357
Shravani Subramanian <ssubra20@calpoly.edu>
2026-05-20 18:24:05 -0700

    Fix package documentation

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,4 +1,12 @@
+#' Split a string
+#'
+#' Split a character string into pieces.
+#' @param x A character vector
+#' @param split What to split on
+#'
+#' @return A list of character vectors
 #' @export
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
+
